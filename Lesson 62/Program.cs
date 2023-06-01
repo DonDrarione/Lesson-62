bb8234862d45dc61e805b15899f2bfdc89fcdca4
sunny <sunny@DESKTOP-UJ1BUSP>
2023-06-02 01:54:50 +0300

    Добавьте файлы проекта.

---
+++ Lesson 62/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("rows and columns = ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+var A_B = GetSpire(rows);
+
+WriteArray(A_B);
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+
+}
+static int[,] GetSpire(int n)
+{
+
+    int[,] sqareMatrix = new int[n, n];
+
+    int temp = 1;
+    int i = 0;
+    int j = 0;
+
+    while (temp <= sqareMatrix.GetLength(0) * sqareMatrix.GetLength(1))
+    {
+        sqareMatrix[i, j] = temp;
+        temp++;
+        if (i <= j + 1 && i + j < sqareMatrix.GetLength(1) - 1)
+            j++;
+        else if (i < j && i + j >= sqareMatrix.GetLength(0) - 1)
+            i++;
+        else if (i >= j && i + j > sqareMatrix.GetLength(1) - 1)
+            j--;
+        else
+            i--;
+    }
+
+return sqareMatrix; 
+}
+
